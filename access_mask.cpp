262e9efb84815a3d7f81821b9c825a118d8bcf68
AntonSuvorov <ant.suvorov@gmail.com>
2019-06-02 23:52:15 +0300

    Update access_mask.cpp

--- access_mask.cpp
+++ access_mask.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <bitset>
 #include <boost/operators.hpp>
 
-#include <iostream>
 
 template<typename AccessMask, unsigned Offset>
 struct AccessMaskElement 
@@ -9,21 +9,21 @@ struct AccessMaskElement
     , boost::subtractable<AccessMaskElement<AccessMask, Offset>
     > >
 {    
-    struct Part1
+    struct Access
     {
         enum 
         {
-            Deny = 1 << Offset - 1,
-            Ask = 2 << Offset - 1,
-            Allow = 3 << Offset - 1,
+            Deny = 1 << Offset,
+            Ask = 1 << (Offset + 1),
+            Allow = 1 << (Offset + 2),
             Mask = Deny | Ask | Allow
         };
     };
-    struct Part2
+    struct Addition
     {
         enum 
         {
-            Log = 4 << Offset -1,
+            Log = 1 << (Offset + 3),
             Mask = Log
         };
     };
@@ -36,13 +36,13 @@ struct AccessMaskElement
     
     AccessMaskElement& operator+=(const AccessMaskElement& right)
     {        
-        static_cast<AccessMask&>(*this).m_mask |= static_cast<AccessMask&>(right).m_mask;        
+        static_cast<AccessMask&>(*this).m_mask |= ElementMask & static_cast<AccessMask&>(right).m_mask;        
         return *this;
     }
     
     AccessMaskElement& operator-=(const AccessMaskElement& right)
     {
-        static_cast<AccessMask&>(*this).m_mask &= ~static_cast<AccessMask&>(right).m_mask;         
+        static_cast<AccessMask&>(*this).m_mask &= ElementMask & ~static_cast<AccessMask&>(right).m_mask;         
         return *this;
     }
 };
@@ -57,12 +57,13 @@ struct AccessMask
     > >
 {
     using PrivilegeMask = AccessMaskElement<AccessMask, 0>;
-    using WriteMask = AccessMaskElement<AccessMask, 0>;
+    using WriteMask = PrivilegeMask;
     using ReadMask = AccessMaskElement<AccessMask, 1>;
     using CreateMask = AccessMaskElement<AccessMask, 2>;
     using DeleteMask = AccessMaskElement<AccessMask, 3>;
     
     uint32_t m_mask = 0;
+    
     
     AccessMask& operator+=(const AccessMask& right)
     {        
@@ -82,6 +83,15 @@ struct AccessMask
 int main()
 {
     AccessMask inherite, allow, ask, deny, log;
-//    allow.m_mask = AccessMask::
+    allow.m_mask = AccessMask::PrivilegeMask::Access::Allow;
+    ask.m_mask = AccessMask::PrivilegeMask::Access::Ask;
+    deny.m_mask = AccessMask::PrivilegeMask::Access::Deny;
+    log.m_mask = AccessMask::PrivilegeMask::Addition::Log;
+    
+    AccessMask tmp(inherite + allow);
+    std::cout << std::bitset<32>(tmp.m_mask) << "\n";
+    
+    tmp -= allow;
+    std::cout << std::bitset<32>(tmp.m_mask) << "\n";
     return 0;
 }
