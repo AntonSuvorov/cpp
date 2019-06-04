638597a992dded2a59c95928bc9ac882b404d014
AntonSuvorov <ant.suvorov@gmail.com>
2019-06-04 06:02:51 +0300

    Update access_mask.cpp

--- access_mask.cpp
+++ access_mask.cpp
@@ -7,7 +7,8 @@ template<typename AccessMask, unsigned Offset>
 struct AccessMaskElement 
     : boost::addable<AccessMaskElement<AccessMask, Offset>
     , boost::subtractable<AccessMaskElement<AccessMask, Offset>
-    > >
+    , boost::equality_comparable<AccessMaskElement<AccessMask, Offset>
+    > > >
 {    
     struct Access
     {
@@ -33,49 +34,75 @@ struct AccessMaskElement
         ElementMask = Access::Mask | Addition::Mask,
     };
         
-    
-    AccessMaskElement& operator+=(const AccessMaskElement& right)
+    operator AccessMask() const noexcept
+    {
+        return AccessMask(ElementMask & static_cast<const AccessMask&>(*this).m_mask);
+    }
+        
+    AccessMaskElement& operator=(const AccessMaskElement& right) noexcept
     {        
-        static_cast<AccessMask&>(*this).m_mask |= ElementMask & static_cast<AccessMask&>(right).m_mask;        
+        static_cast<AccessMask&>(*this).m_mask = ElementMask & static_cast<const AccessMask&>(right).m_mask;        
         return *this;
     }
     
-    AccessMaskElement& operator-=(const AccessMaskElement& right)
-    {
-        static_cast<AccessMask&>(*this).m_mask &= ElementMask & ~static_cast<AccessMask&>(right).m_mask;         
+    AccessMaskElement& operator+=(const AccessMaskElement& right) noexcept
+    {        
+        static_cast<AccessMask&>(*this).m_mask |= ElementMask & static_cast<const AccessMask&>(right).m_mask;        
         return *this;
+    }
+    
+    AccessMaskElement& operator-=(const AccessMaskElement& right) noexcept
+    {
+        static_cast<AccessMask&>(*this).m_mask &= ~(ElementMask & static_cast<const AccessMask&>(right).m_mask);         
+        return *this;
+    }
+    
+    bool operator==(const AccessMaskElement& right) noexcept
+    {
+        return (ElementMask & static_cast<AccessMask&>(*this).m_mask) == (ElementMask & static_cast<const AccessMask&>(right).m_mask);         
     }
 };
 
 struct AccessMask 
     : AccessMaskElement<AccessMask, 0>
-    , AccessMaskElement<AccessMask, 1>
-    , AccessMaskElement<AccessMask, 2>
-    , AccessMaskElement<AccessMask, 3>
+    , AccessMaskElement<AccessMask, 4>
+    , AccessMaskElement<AccessMask, 8>
+    , AccessMaskElement<AccessMask, 12>
     , boost::addable<AccessMask
     , boost::subtractable<AccessMask
-    > >
+    , boost::equality_comparable<AccessMask
+    > > >
 {
     using PrivilegeMask = AccessMaskElement<AccessMask, 0>;
     using WriteMask = PrivilegeMask;
-    using ReadMask = AccessMaskElement<AccessMask, 1>;
-    using CreateMask = AccessMaskElement<AccessMask, 2>;
-    using DeleteMask = AccessMaskElement<AccessMask, 3>;
+    using ReadMask = AccessMaskElement<AccessMask, 4>;
+    using CreateMask = AccessMaskElement<AccessMask, 8>;
+    using DeleteMask = AccessMaskElement<AccessMask, 12>;
     
-    uint32_t m_mask = 0;
+    uint16_t m_mask = 0;
     
+    AccessMask() = default;
     
-    AccessMask& operator+=(const AccessMask& right)
+    explicit AccessMask(uint16_t mask) noexcept
+        : m_mask(mask)
+    {}
+    
+    AccessMask& operator+=(const AccessMask& right) noexcept
     {        
         m_mask |= right.m_mask;        
         return *this;
     }
     
-    AccessMask& operator-=(const AccessMask& right)
+    AccessMask& operator-=(const AccessMask& right) noexcept
     {
         m_mask &= ~right.m_mask;         
         return *this;
     }    
+    
+    bool operator==(const AccessMask& right) noexcept
+    {
+        return m_mask == right.m_mask;         
+    }
 };
 
 
@@ -89,9 +116,20 @@ int main()
     log.m_mask = AccessMask::PrivilegeMask::Addition::Log;
     
     AccessMask tmp(inherite + allow);
-    std::cout << std::bitset<32>(tmp.m_mask) << "\n";
+    std::cout << std::bitset<16>(tmp.m_mask) << "\n";
     
     tmp -= allow;
-    std::cout << std::bitset<32>(tmp.m_mask) << "\n";
+    std::cout << std::bitset<16>(tmp.m_mask) << "\n";
+    
+    static_cast<AccessMask::PrivilegeMask&>(tmp) += static_cast<const AccessMask::PrivilegeMask&>(ask);
+    std::cout << std::bitset<16>(tmp.m_mask) << "\n";
+    
+    static_cast<AccessMask::PrivilegeMask&>(tmp) -= ask;
+    std::cout << std::bitset<16>(tmp.m_mask) << "\n";
+    
+    tmp += static_cast<const AccessMask::PrivilegeMask&>(ask);
+    std::cout << std::bitset<16>(tmp.m_mask) << "\n";
+    
+    allow == ask;
     return 0;
 }
